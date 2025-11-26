96741864356520cb344fd1180d35a4bd46f6cac9
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2025-11-26 23:33:09 +0700

    Update myarfunc.c

--- myarfunc.c
+++ myarfunc.c
@@ -1,28 +1,24 @@
-// myarrfunc.c
+
 #include <stdio.h>
 #include "myarrfunc.h"
-
 // Создание массива
 void sozdat(int massiv[], int razmer) {
     if (razmer <= 0) {
         printf("Ошибка, введите значение больше 0\n");
         return;
     }
-    
     // Инициализируем массив нулями
     for (int i = 0; i < razmer; i++) {
         massiv[i] = 0;
     }
     printf("массив размером %d готов\n", razmer);
 }
-
 // Заполнение массива
 void zapolnit(int massiv[], int razmer) {
     if (razmer <= 0) {
         printf("ошибка, создайте массив\n");
         return;
-    }
-    
+    }   
     printf("заполните массив из %d элементов:\n", razmer);
     for (int i = 0; i < razmer; i++) {
         printf("элемент [%d]: ", i);
@@ -30,7 +26,6 @@ void zapolnit(int massiv[], int razmer) {
     }
     printf("массив заполнен\n");
 }
-
 // Вывод массива (значения и адреса)
 void pokazat(int massiv[], int razmer) {
     if (razmer <= 0) {
@@ -41,80 +36,69 @@ void pokazat(int massiv[], int razmer) {
         printf("%6d | %9d | %p\n", i, massiv[i], (void*)&massiv[i]);
     }
 }
-
 // Поиск минимального значения в диапазоне [k, m]
 int nayti_min_v_diapazone(int massiv[], int razmer, int k, int m) {
     if (razmer <= 0) {
         printf("ошибка, создайте массив\n");
         return -1;
-    }
-    
+    }   
     if (k < 0 || m >= razmer || k > m) {
-        printf("Oshibka: neverny diapazon!\n");
+        printf("ошибка, неверный диапазон\n");
         return -1;
     }
-    
     int min = massiv[k];
-    for (int i = m + 1; i <= konec; i++) {
-        if (massiv[i] < minimum) {
-            minimum = massiv[i];
+    for (int i = m + 1; i <= m; i++) {
+        if (massiv[i] < min) {
+            min = massiv[i];
         }
     }
-    return minimum;
+    return min;
 }
-
 // Количество элементов между двумя равными числами
-int schitat_mezhdu_ravnimi(int massiv[], int razmer, int iskomoe) {
+int schitat_mezhdu(int massiv[], int razmer, int iskom) {
     if (razmer <= 0) {
-        printf("Oshibka: massiv ne sozdan!\n");
+        printf("ошибка, создайте массив\n");
         return -1;
-    }
-    
+    }   
     int pervy_index = -1, posledny_index = -1;
-    
     // Находим первое вхождение
     for (int i = 0; i < razmer; i++) {
-        if (massiv[i] == iskomoe) {
+        if (massiv[i] == iskom) {
             pervy_index = i;
             break;
         }
     }
-    
     // Находим последнее вхождение
     for (int i = razmer - 1; i >= 0; i--) {
-        if (massiv[i] == iskomoe) {
+        if (massiv[i] == iskom) {
             posledny_index = i;
             break;
         }
     }
-    
     if (pervy_index == -1 || posledny_index == -1 || pervy_index == posledny_index) {
-        printf("Chislo %d ne naideno ili naideno tolko odin raz\n", iskomoe);
+        printf("число %d не найдено или найдено только один раз\n", iskom);
         return 0;
     }
-    
-    int kolichestvo = posledny_index - pervy_index - 1;
-    return (kolichestvo > 0) ? kolichestvo : 0;
+    int kolich = posledny_index - pervy_index - 1;
+    return (kolich > 0) ? kolichestvo : 0;
 }
-
 // Поиск индексов элементов меньше granica
-void nayti_indexy_menche(int massiv[], int razmer, int granica) {
+void nayti_indexy(int massiv[], int razmer, int granica) {
     if (razmer <= 0) {
-        printf("Oshibka: massiv ne sozdan!\n");
+        printf("ошибка, создайте массив\n");
         return;
-    }
-    
-    printf("Indexy elementov menche %d: ", granica);
-    int naideno = 0;
+    }   
+    printf("индекс элементов меньше %d: ", granica);
+    int naiden = 0;
     for (int i = 0; i < razmer; i++) {
         if (massiv[i] < granica) {
             printf("%d ", i);
-            naideno = 1;
+            naiden = 1;
         }
     }
     
-    if (!naideno) {
-        printf("ne naideno");
+    if (!naiden) {
+        printf("не найдено");
     }
     printf("\n");
 }
